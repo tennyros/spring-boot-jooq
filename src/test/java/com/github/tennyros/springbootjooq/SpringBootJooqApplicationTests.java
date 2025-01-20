9a0bb135eb9a29234e49e1d22b29fde6efaf8800
wendelca <vvkamalov_obs@mail.ru>
2025-01-20 18:48:39 +0500

    init commit

---
+++ src/test/java/com/github/tennyros/springbootjooq/SpringBootJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.springbootjooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJooqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
